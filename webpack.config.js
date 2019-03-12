9c4f26adbb1b1ed5057a2cdcc28ade09df966150
chenyin <cyin@163.com>
2019-03-13 02:09:30 +0800

    接着上个版本，js文件名称是动态生成的，我们在index.html中引入脚本就不能写成静态文件名，例如：<script type="text/javascript" src="js/bundle.js"></script>,那么我们怎么办呢，这个时候我们就需要引入一个插件(html-webpack-plugin)：npm i html-webpack-plugin --save-dev,然后在plugin中实例化html-webpack-plugin，这个时候就会动态生成一个index.html文件，里面的script引入的js文件的名称就是动态的。这个时候我们若想把自己写的index.html引入动态名称脚本文件，我们要给html-webpack-plugin实例对象传递template参数，并把我们的模板路径写入

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 // webpack的配置文件
+// 引入html-webpack-plugin插件
+var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     // 打包的入口是从哪里开始,这里不能用./,要用__dirname来获取当前文件的路径，entry有3中表现形式：
     // 1.字符串：这个适用于单个文件
@@ -8,7 +10,7 @@ module.exports = {
     // entry: [__dirname + '/src/script/main.js', __dirname + '/src/script/a.js'],
     // 这种写法其实等价于数组写法,但是区别是给了数组写法起了一个名字，在下面的output中可以动态修改输出文件名
     entry: {
-        'main': [__dirname + '/src/script/main.js', __dirname + '/src/script/a.js']
+        'main1': [__dirname + '/src/script/main.js', __dirname + '/src/script/a.js']
     },
     // entry: {
     //     'main': __dirname + '/src/script/main.js',
@@ -17,9 +19,20 @@ module.exports = {
     // 输出文件
     output: {
         // 打包的文件放在哪个目录下
-        path: __dirname + '/dist/js',
+        path: __dirname + '/dist',
         // 打包的文件名
         // filename: '[name]-[hash].js'
-        filename: '[name].js'
-    }
+        filename: 'js/[name]-[chunkhash].js'
+    },
+    // 引用插件的地方，我们在这里new htmlWebpackPlugin(),则会生成一个HTML文件，但是这个文件与我们自己写的index.html 
+    // 没什么关联，若我们想用自己的index.html则要给它设置template
+    plugins: [
+        new htmlWebpackPlugin({
+            // 可以给生成的html指定名称
+            filename: 'index-[hash].html',
+            template:'index.html',
+            // script标签是放在head还是在body中
+            inject: 'body'
+        })
+    ]
 }
