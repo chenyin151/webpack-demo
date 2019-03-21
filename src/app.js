4e9f37d0dcc62698434f00f722129cf199346168
chenyin <cyin@163.com>
2019-03-22 02:12:37 +0800

    这里是配置babel， 让webpack识别es6语法，注意： 1.新版loaders->rules,loader不能用简写的方式，必须要写成babel-loader,presets也不能用简写方式，必须用@babel/preset-env这种方式 2.query->options 必须用npm install -D babel-loader @babel/core @babel/preset-env webpack的方式安装babel环境

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import layer from './components/layer/layer.js';
+
+const App = function() {
+    const NUM = 11112;
+    alert(NUM)
+    console.log(layer);
+}
+new App();
--- webpack.config.js
+++ webpack.config.js
@@ -11,80 +11,36 @@ module.exports = {
     // entry: __dirname + '/src/script/main.js',
     // entry: [__dirname + '/src/script/main.js', __dirname + '/src/script/a.js'],
     // 这种写法其实等价于数组写法,但是区别是给了数组写法起了一个名字，在下面的output中可以动态修改输出文件名
-    entry: {
-        // 'main1': [__dirname + '/src/script/main.js', __dirname + '/src/script/a.js'],
-        // 'main2': __dirname + '/src/script/main.js',
-        'main': __dirname + '/src/script/main.js',
-        'a': __dirname + '/src/script/a.js',
-        'b': __dirname + '/src/script/b.js',
-        'c': __dirname + '/src/script/c.js'
-    },
+    entry: './src/app.js',
     // entry: {
     //     'main': __dirname + '/src/script/main.js',
     //     'a': __dirname + '/src/script/a.js'
     // },
     // 输出文件
     output: {
-        // 打包的文件放在哪个目录下
-        path: __dirname + '/dist',
-        // 打包的文件名
-        // filename: '[name]-[hash].js'
-        filename: 'js/[name]_[chunkhash].js',
-        // 发布上线后的服务器地址，这样一旦发布成功，项目根目录会自动替换成http://cdn.con/
-        publicPath: 'http://cdn.com/'
+       path: __dirname + '/dist',
+       filename: 'js/[name].bundle.js'
+    },
+    // 模块加载器,当检测是js文件则运用babel解析器去解析js代码，query指定了用babel来解析哪个版本的ES文件
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
     },
     // 引用插件的地方，我们在这里new htmlWebpackPlugin(),则会生成一个HTML文件，但是这个文件与我们自己写的index.html 
     // 没什么关联，若我们想用自己的index.html则要给它设置template
     plugins: [
         new htmlWebpackPlugin({
-            // 可以给生成的html指定名称
-            filename: 'a.html',
-            template:'index.html',
-            // script标签是放在head还是在body中
-            // inject: 'body',
-            inject:false,
-            // inject: 'body',
-            title: 'this is a.html',
-            date: new Date(),
-            // chunks: ['a']
-            excludeChunks: ['b', 'c']  //excludeChunks是除去我指定的几个chunks之外的chunks
-            // 发布会压缩文件
-            // minify: {
-            //     removeComments: true, //删除注释
-            //     collapseWhitespace: true //删除空格
-            // }
-        }),
-        new htmlWebpackPlugin({
-            // 可以给生成的html指定名称
-            filename: 'b.html',
-            template:'index.html',
-            // script标签是放在head还是在body中
-            // inject: 'body',
-            inject: false,
-            title: 'this is b.html',
-            date: new Date(),
-            excludeChunks: ['a','c']
-            // 发布会压缩文件
-            // minify: {
-            //     removeComments: true, //删除注释
-            //     collapseWhitespace: true //删除空格
-            // }
-        }),
-        new htmlWebpackPlugin({
-            // 可以给生成的html指定名称
-            filename: 'c.html',
-            template:'index.html',
-            // script标签是放在head还是在body中
-            // inject: 'body',
-            inject: false,
-            title: 'this is c.html',
-            date: new Date(),
-            excludeChunks: ['a','b']
-            // 发布会压缩文件
-            // minify: {
-            //     removeComments: true, //删除注释
-            //     collapseWhitespace: true //删除空格
-            // }
+            filename: 'index.html',
+            template: 'index.html',
+            inject: 'body'
         })
     ]
 }
